7cce92f0ae0ec3ad8d4fd4e8fee20e55fcae8dc9
vulffff <olivermarkus.vulf@gmail.com>
2024-11-23 21:34:30 +0200

    added password validation and its styling

---
+++ homework3/src/assets/js/validatePass.js
@@ -0,0 +1,34 @@
+// Function that collects password validation error messages into an array and returns them.
+export function validatePass (password) {
+    let errors = [];
+
+    if(!password) {
+        errors.push("Password is required!");
+    }
+
+    if (password.length < 8 || password.length >= 15) {
+        errors.push("Password must be of a specific length (at least 8 chars and less than 15 chars)");
+    }
+
+    if(!/[A-Z]/.test(password)) {
+        errors.push("Password must include at least one uppercase alphabet character");
+    }
+
+    if ((password.match(/[a-z]/g) || []).length < 2 ) {
+        errors.push("Password must include at least two lowercase alphabet characters.");
+    }
+
+    if (!/[A-Z]/.test(password)) {
+        errors.push("Password must include at least one numeric value");
+    }
+
+    if (!/^[A-Z]/.test(password)) {
+        errors.push("Password must start with an uppercase alphabet character");
+    }
+
+    if (!/_/.test(password)) {
+        errors.push("Password must include the character '_'")
+    }
+
+    return errors;
+}
--- homework3/src/views/SignUpView.vue
+++ homework3/src/views/SignUpView.vue
@@ -3,10 +3,15 @@
         <h2>Welcome to PostIt</h2>
         <h2>Sign up</h2>
         <div class="form-container">
-        <form class="custom-form" action="index.html">
-            <input type="email" name="email" id="email" placeholder="email" required><br>
-            <input type="password" name="password" id="password" placeholder="password" required><br>
-            <input type="submit" value="log in"><br>
+        <form class="custom-form" @submit="handleSubmit">
+            <input type="email" name="email" id="email" placeholder="email" v-model="email" required><br>
+            <input type="password" name="password" id="password" placeholder="password" v-model="password" required><br>
+            
+            <ul class="validation-errors" v-if="validationErrors.length > 0">
+                <li v-for="(error,index) in validationErrors" :key="index" class = "error-message">{{ error }}</li>
+            </ul>
+            
+            <input type="submit"  value="Sign up" /><br />
         </form><br>
         </div>
     </div>
@@ -14,7 +19,25 @@
     
     
 <script>
+import {validatePass} from '@/assets/js/validatePass'; 
     export default {
-        name: "SignUpView"
+        name: "SignUpView",
+        data() {
+            return {
+                email: "",
+                password: "",
+                validationErrors: [],
+            };
+        },
+        methods: {
+            handleSubmit() {
+                this.validationErrors = validatePass(this.password);
+                if (this.validationErrors.length === 0) {
+                    alert("Signup successful!");
+                } else {
+                    event.preventDefault(); // Stops the default form submission and page reload to display error messages.
+                }
+            },
+        },
     };
 </script>
